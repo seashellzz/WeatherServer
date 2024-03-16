22b14b38c0db0895d65856e75a37c3135364a094
Seashellzz <xzzhannah@gmail.com>
2024-03-15 20:52:15 -0700

    updates for cities

---
+++ WeatherServer/Dto/CountryPopulation.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherServer.Dto
+{
+    public class CountryPopulation
+    {
+        public int countryId { get; set; }
+        public required string countryName { get; set; }
+
+        public int population { get; set; }
+    }
+}
