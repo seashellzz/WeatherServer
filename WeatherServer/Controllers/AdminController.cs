8064d37c1878807229efd88ea1f4a9b41999df4c
Seashellzz <xzzhannah@gmail.com>
2024-04-19 20:41:28 -0700

    adding loginRequest and LoginResult

--- WeatherServer/Controllers/AdminController.cs
+++ WeatherServer/Controllers/AdminController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using WeatherServer.Dto;
 
 namespace WeatherServer.Controllers
 {
@@ -13,10 +15,29 @@ namespace WeatherServer.Controllers
         jwtHandler jwtHandler) : ControllerBase
     {
         //get user name, password to generate token
-        [HttpPost]
-        public void Login() 
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginRequest loginRequest) 
         {
-
+            WorldCityUser? user = await userManager.FindByNameAsync(loginRequest.UserName);
+            // ? means it could be null
+            if(user == null)
+            {
+                return Unauthorized("Bad username");
+            }
+            bool success = await userManager.CheckPasswordAsync(user, loginRequest.Password);
+            if(!success)
+            {
+                return Unauthorized("Wrong password");
+            }
+            JwtSecurityToken token = await jwtHandler.GetTokenAsync(user); //generate token for user
+            string jwtstring = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(new LoginResult
+            {
+                Success = true,
+                Message = "Login Success",
+                Token = jwtstring
+            });
+           
         }
     }
 }
