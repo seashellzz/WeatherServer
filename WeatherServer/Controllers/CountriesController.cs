46d9d0dfb011acaaad4f4694e7dd912f8bc60e43
Seashellzz <xzzhannah@gmail.com>
2024-04-26 20:40:14 -0700

    adding authorization to getCitiesByCountry

--- WeatherServer/Controllers/CountriesController.cs
+++ WeatherServer/Controllers/CountriesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CountryModel;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WeatherServer.Controllers
 {
@@ -37,6 +38,7 @@ namespace WeatherServer.Controllers
 
         // GET: api/Countries
         [HttpGet("CountryCities/{id}")]
+        [Authorize] 
         public async Task<ActionResult<IEnumerable<City>>> GetCitiesByCountry(int id)
         {
             return await context.Cities.Where(c => c.CountryId == id).ToListAsync();
